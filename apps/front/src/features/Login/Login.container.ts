36414d180920dbee467f06d309da82d184bbb795
felipetmacedo <ftm2@cin.ufpe.br>
2025-06-10 14:40:04 -0300

    fix: usuario inválido

--- apps/front/src/features/Login/Login.container.ts
+++ apps/front/src/features/Login/Login.container.ts
@@ -1,54 +1,63 @@
 import * as yup from 'yup';
-import { useState } from "react";
-import { toast } from "sonner";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { getUserLojas } from "@/processes/auth";
-import { loginSchema } from "@/schemas/auth";
-import { useLojasStore } from "@/stores/lojas.store";
+import { useState } from 'react';
+import { toast } from 'sonner';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { getUserLojas } from '@/processes/auth';
+import { loginSchema } from '@/schemas/auth';
+import { useLojasStore } from '@/stores/lojas.store';
 
 type LoginFormData = yup.InferType<typeof loginSchema>;
 
 export default function LoginContainer() {
-  const navigate = useNavigate();
-  const [isLoading, setIsLoading] = useState(false);
-  const [showPassword, setShowPassword] = useState(false);
-  const { setLojas } = useLojasStore();
+	const navigate = useNavigate();
+	const [isLoading, setIsLoading] = useState(false);
+	const [showPassword, setShowPassword] = useState(false);
+	const { setLojas } = useLojasStore();
 
-  const { register, handleSubmit, formState: { errors } } = useForm<LoginFormData>({
-    resolver: yupResolver(loginSchema)
-  });
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<LoginFormData>({
+		resolver: yupResolver(loginSchema),
+	});
 
-  const onSubmit = async (data: LoginFormData) => {
-    try { 
-      setIsLoading(true);
+	const onSubmit = async (data: LoginFormData) => {
+		try {
+			setIsLoading(true);
 
-      try {
-        const response = await getUserLojas({
-          usuario: data.usuario,
-          senha: data.senha
-        });
+			try {
+				const response = await getUserLojas({
+					usuario: data.usuario,
+					senha: data.senha,
+				});
 
-        setLojas(response);
+				if (response.length === 0) {
+					toast.error('Usuário não cadastrado');
+					return;
+				}
 
-        navigate("/dashboard");
-        toast.success("Login realizado com sucesso!");
-      } catch {
-        toast.error("Token inválido!");
-      }
-    } finally {
-      setIsLoading(false);
-    }
-  };
+				setLojas(response);
 
-  return {
-    register,
-    errors,
-    onSubmit,
-    handleSubmit,
-    isLoading,
-    showPassword,
-    setShowPassword
-  }
+				navigate('/dashboard');
+				toast.success('Login realizado com sucesso!');
+			} catch {
+				toast.error('Token inválido!');
+			}
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	return {
+		register,
+		errors,
+		onSubmit,
+		handleSubmit,
+		isLoading,
+		showPassword,
+		setShowPassword,
+	};
 }
